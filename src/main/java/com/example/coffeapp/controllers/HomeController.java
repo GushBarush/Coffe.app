f8f465e25834905c70387dad54b17be4dc0fa3bc
GushBarush <GushBarush1@gmail.com>
2021-11-16 11:52:22 +0300

    Add Security

---
+++ src/main/java/com/example/coffeapp/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.coffeapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.Map;
+
+@Controller
+public class HomeController {
+
+    @GetMapping()
+    public String greeting(Map<String, Object> model) {
+        return "home";
+    }
+}
