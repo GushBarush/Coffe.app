dbbaef2d3e888539b0f766e280173266e9b71cc8
GushBarush <GushBarush1@gmail.com>
2021-11-16 11:53:31 +0300

    Add Security

---
+++ src/main/java/com/example/coffeapp/controllers/MessageController.java
@@ -0,0 +1,54 @@
+package com.example.coffeapp.controllers;
+
+import com.example.coffeapp.models.Message;
+import com.example.coffeapp.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.Map;
+
+@Controller()
+public class MessageController {
+
+    @Autowired
+    private MessageRepo messageRepo;
+
+    @GetMapping("/message")
+    public String main(Map<String, Object> model) {
+        Iterable<Message> messages = messageRepo.findAll();
+
+        model.put("messages", messages);
+
+        return "message";
+    }
+
+    @PostMapping("/message")
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model){
+        Message message = new Message(text, tag);
+
+        messageRepo.save(message);
+
+        Iterable<Message> messages = messageRepo.findAll();
+
+        model.put("messages", messages);
+
+        return "message";
+    }
+
+    @PostMapping("/message/filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+        Iterable<Message> messages;
+
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+
+        model.put("messages", messages);
+
+        return "message";
+    }
+}
