865ed9f30627eb8b8dcff0d92ba056980d190dc2
GushBarush <GushBarush1@gmail.com>
2022-01-24 15:18:16 +0300

    save

---
+++ src/main/resources/db.migration/V2__addValues.sql
@@ -0,0 +1,7 @@
+INSERT INTO "role" (role_name) VALUES ('ADMIN');
+INSERT INTO "role" (role_name) VALUES ('USER');
+INSERT INTO "role" (role_name) VALUES ('BARISTA');
+
+INSERT INTO "size" (size_name) VALUES ('SMALL');
+INSERT INTO "size" (size_name) VALUES ('MEDIUM');
+INSERT INTO "size" (size_name) VALUES ('BIG');
