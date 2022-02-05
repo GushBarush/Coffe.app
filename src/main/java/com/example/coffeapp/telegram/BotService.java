e7c8130d488d5141deb4f6333d230eba72cfbc7e
GushBarush <GushBarush1@gmail.com>
2022-02-05 12:29:52 +0300

    save

--- src/main/java/com/example/coffeapp/telegram/BotService.java
+++ src/main/java/com/example/coffeapp/telegram/BotService.java
@@ -17,12 +17,12 @@ public class BotService {
         PayDay payDay = payDayRepo.findByActive(true);
 
         if (payDay != null){
-            stats = "Cмену открыл: " + payDay.getUser().getName() + ".\n" +
-                    "В " + payDay.getOpenTime().getHour() + ":" + payDay.getOpenTime().getMinute() + ".\n" +
-                    "Всего: " + payDay.getSumAll() + ".\n" +
-                    "Расходы: " + payDay.getSumExpense() + ".\n" +
-                    "Наличные: " + payDay.getSumCash() + ".\n" +
-                    "Безнал: " + payDay.getSumNotCash() + ".\n" +
+            stats = "Cмену открыл: " + payDay.getUser().getName() + ". " +
+                    "В " + payDay.getOpenTime().getHour() + ":" + payDay.getOpenTime().getMinute() + ". " +
+                    "Всего: " + payDay.getSumAll() + ". " +
+                    "Расходы: " + payDay.getSumExpense() + ". " +
+                    "Наличные: " + payDay.getSumCash() + ". " +
+                    "Безнал: " + payDay.getSumNotCash() + ". " +
                     "Бесплатных: " + payDay.getSumFree() + ".";
         }
 
