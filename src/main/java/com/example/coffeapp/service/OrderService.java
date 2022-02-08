d0d914f93041bb9ff6342f6914c233922f8ce991
GushBarush <GushBarush1@gmail.com>
2022-02-08 18:08:14 +0300

    save

--- src/main/java/com/example/coffeapp/service/OrderService.java
+++ src/main/java/com/example/coffeapp/service/OrderService.java
@@ -186,7 +186,7 @@ public class OrderService {
         expense.setPayDay(payDay);
 
         payDay.setSumExpense(payDay.getSumExpense() + expense.getSum());
-        payDay.setSumAll(payDay.getSumCash() - expense.getSum());
+        payDay.setSumCash(payDay.getSumCash() - expense.getSum());
 
         payDayRepo.save(payDay);
         expenseRepo.save(expense);
@@ -196,7 +196,7 @@ public class OrderService {
         Expense expense = expenseRepo.getById(expenseId);
         PayDay payDay = payDayRepo.getById(expense.getPayDay().getId());
 
-        payDay.setSumAll(payDay.getSumCash() + expense.getSum());
+        payDay.setSumCash(payDay.getSumCash() + expense.getSum());
         payDay.setSumExpense(payDay.getSumExpense() - expense.getSum());
 
         payDayRepo.save(payDay);
